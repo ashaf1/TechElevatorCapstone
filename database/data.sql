e4015dae4be6ea4fa8d17c2c3277014f38a100fd
Anthony Valentino <anthonyvalentino@Techs-Air-9.att.net>
2016-08-08 14:53:00 -0400

    Initial database data setup

--- database/data.sql
+++ database/data.sql
@@ -4,6 +4,31 @@
 
 BEGIN;
 
--- INSERT statements go here
+INSERT INTO user_table(role, first_name, last_name, user_name, password, salt)
+VALUES('Administrator', 'Anthony', 'Valentino', 'AValentino', 'password', 'pepper');
+
+INSERT INTO user_table(role, first_name, last_name, user_name, password, salt)
+VALUES('Employee', 'Callum', 'Orr', 'COrr', 'callum', '$12ffsdf');
+
+INSERT INTO user_table(role, first_name, last_name, user_name, password, salt)
+VALUES('Employee', 'Aaron', 'Shafron', 'AShafron', 'aaron', 'gitpush');
+
+INSERT INTO location(street_address, city, state, zip, comments)
+VALUES('7100 Euclid Ave', 'Cleveland', 'OH', '44103', 'Located right outside in front of the building.');
+
+INSERT INTO location(street_address, city, state, zip)
+VALUES('123 Smith Rd', 'Brunswick', 'OH', '44212');
+
+INSERT INTO location(street_address, city, state, zip, comments)
+VALUES('Main St', 'Stow', 'OH', '44224', 'Located between the red house and the green building.');
+
+INSERT INTO pothole(location_id)
+VALUES (1);
+
+INSERT INTO pothole(location_id)
+VALUES (2);
+
+INSERT INTO pothole(location_id)
+VALUES (3);
 
 COMMIT;
--- database/schema.sql
+++ database/schema.sql
@@ -4,6 +4,40 @@
 
 BEGIN;
 
--- CREATE statements go here
+CREATE TABLE user_table (
+	user_id serial NOT NULL,
+	role varchar(64) NOT NULL,
+	first_name varchar(64) NOT NULL,
+	last_name varchar(64) NOT NULL,
+	user_name varchar(32) NOT NULL,
+	password varchar(32) NOT NULL,
+	salt varchar(256) NOT NULL,
+	CONSTRAINT pk_user_id PRIMARY KEY (user_id)
+);
+
+CREATE TABLE location (
+	location_id serial NOT NULL,
+	street_address varchar(64) NOT NULL,
+	city varchar(64) NOT NULL,
+	state varchar(2) NOT NULL,
+	zip varchar(12) NOT NULL,
+	comments varchar(300),
+	CONSTRAINT pk_location_id PRIMARY KEY (location_id)
+);
+
+CREATE TABLE pothole (
+	pothole_id serial NOT NULL,
+	location_id integer NOT NULL,
+	is_scheduled boolean NOT NULL DEFAULT FALSE,
+	has_been_visited boolean NOT NULL DEFAULT FALSE,
+	is_duplicate boolean NOT NULL DEFAULT FALSE,
+	is_fixed boolean NOT NULL DEFAULT FALSE,
+	priority_level varchar(32),
+	create_date date NOT NULL DEFAULT now(),
+	inspection_date date,
+	fixed_date date,
+	CONSTRAINT pk_pothole_id PRIMARY KEY (pothole_id),
+	FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
 
 COMMIT;
